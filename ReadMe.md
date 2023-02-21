1ae939b9f7f7e31f3710059478f1128fc1c11150
RyannKim327 <rksesgundo123@gmail.com>
2023-02-21 08:39:25 +0800

    02-21-23 08:39 [Initial Commit]

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+### BMI GUI
+#### MPOP Reverse II (Ryann Kim Sesgundo)
+---
+> This is just a simple program useing JFrame and Maven Projects
+---
+### Application Used
+1. Netbeans
---
+++ src/main/java/mpop/revii/application/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mpop.revii.application;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+    public static void main(String[] srgs){
+	MainFrame main = new MainFrame();
+	main.setTitle("BMI GUI Project");
+	main.setVisible(true);
+    }
+}
